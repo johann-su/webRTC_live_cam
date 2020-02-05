8345dde1f72550e26cd3c362c708190abb8995ae
“[Johann <[johann123.js@gmail.com]>
2020-02-05 13:48:23 +0100

    fixed play button issue

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audi
 
     const signalhub = require('signalhub')
     const hub = signalhub('video-livestream', [
-        'https://192.168.178.42:8080'
+        'https://192.168.2.102:8080'
     ])
 
     // an error has occurred
@@ -49,69 +49,70 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audi
 
     // video stream -> create video and play it
     
-        const video = document.createElement('video')
-        document.body.appendChild(video)
+        // const video = document.createElement('video')
+        // document.body.appendChild(video)
 
-        video.srcObject = stream
-        video.play()
+        // video.srcObject = stream
+        // video.play()
     
 })
 .catch((stream) => {
     // document.write(err)
 
-console.log("Hallo es geht.")
-const Peer = require('simple-peer')
-const peer = new Peer ({
-    initiator: location.hash == '#init',
-    trickle: false,
-})
+    console.log("Hallo es geht.")
+    const Peer = require('simple-peer')
+    const peer = new Peer ({
+        initiator: location.hash == '#init',
+        trickle: false,
+    })
 
-const signalhub = require('signalhub')
-const hub = signalhub('video-livestream', [
-    'https://192.168.178.42:8080'
-])
-peer.on('error', (err) => document.write(err))
+    const signalhub = require('signalhub')
+    const hub = signalhub('video-livestream', [
+        'https://192.168.2.102:8080'
+    ])
+    peer.on('error', (err) => document.write(err))
 
-// when a signal is created -> if initiator = true at start, initiator = false after receiving the offer
-peer.on('signal', (data) => {
-    if(location.hash == '#init') {
-        hub.broadcast('forward', JSON.stringify(data))
-    } else {
-        hub.broadcast('backward', JSON.stringify(data))
-    }
-})
+    // when a signal is created -> if initiator = true at start, initiator = false after receiving the offer
+    peer.on('signal', (data) => {
+        if(location.hash == '#init') {
+            hub.broadcast('forward', JSON.stringify(data))
+        } else {
+            hub.broadcast('backward', JSON.stringify(data))
+        }
+    })
 
-// listens to broadcasted changes from the peer.on('signal) -> uses offer to create awnser
-hub.subscribe('forward').on('data', (data) => {
-    let otherID = JSON.parse(data)
-    if(location.hash != '#init') {
-        console.log('forward', otherID)
-        peer.signal(otherID)
-    }
-})
+    // listens to broadcasted changes from the peer.on('signal) -> uses offer to create awnser
+    hub.subscribe('forward').on('data', (data) => {
+        let otherID = JSON.parse(data)
+        if(location.hash != '#init') {
+            console.log('forward', otherID)
+            peer.signal(otherID)
+        }
+    })
 
-hub.subscribe('backward').on('data', (data) => {
-    let otherID = JSON.parse(data)
-    if(location.hash == '#init') {
-        console.log('backward', otherID)
-        peer.signal(otherID)
-    }
-})
+    hub.subscribe('backward').on('data', (data) => {
+        let otherID = JSON.parse(data)
+        if(location.hash == '#init') {
+            console.log('backward', otherID)
+            peer.signal(otherID)
+        }
+    })
 
-peer.on('connected', () => console.log('IT WORKS :)'))
+    peer.on('connected', () => console.log('IT WORKS :)'))
 
-// video stream -> create video and play it
-peer.on('stream', (stream) => {
-    const video = document.createElement('video')
-    document.body.appendChild(video)
+    // video stream -> create video and play it
+    peer.on('stream', (stream) => {
+        const video = document.createElement('video')
+        document.body.appendChild(video)
 
-    video.srcObject = stream
-    document.querySelector('button').addEventListener('click', function() {
+        video.srcObject = stream
         video.play()
-        console.log("play");
-        document.getElementById("play").style.display = "none"; 
-      });
-})
+        // document.querySelector('button').addEventListener('click', function() {
+        //     video.play()
+        //     console.log("play");
+        //     document.getElementById("play").style.display = "none"; 
+        //   });
+    })
 })
 
 
